2ba48fd65bdcbd1a24e82f8ddd1b91ac194a881a
Vinaya432 <vinayam432@gmail.com>
2024-03-06 12:10:08 +0530

    TodoList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDo-List';
+
+  tasks:string=''
+  allTasks:any=[]
+  editTask:string=''
+  editTaskIndex:number=-1
+
+  handleAdd(){
+    if(this.tasks){
+      console.log(this.tasks);
+      this.allTasks.push(this.tasks);
+      console.log(this.allTasks);
+      this.tasks=''
+      
+    }
+  }
+
+  handleRemove(index:number){
+    this.allTasks.splice(index, 1);
+  }
+
+  openModal(task: string, index: number) {
+    this.editTask = task;
+    this.editTaskIndex=index
+    
+  }
+
+  saveEdit(){
+    if (this.editTaskIndex !== -1) {
+      this.allTasks[this.editTaskIndex] = this.editTask;
+    }
+    this.editTask = '';
+    this.editTaskIndex = -1; 
+
+  }
+
 }
